784e63aa4d0b3db4500a653dc66eb5dea2341cb3
이시우 Fredric <cu.li@fadutec.com>
2023-01-05 15:58:46 +0900

    error in PV0

--- sldensity/PRHL.py
+++ sldensity/PRHL.py
@@ -180,8 +180,8 @@ def trc_logis(df):
     df = df0(df)
     try:
         res = minimize(SS_trc_logis, x0=[5,4], method = 'Powell',args=(df,), bounds=([-5,30],[3,8]))
-    except ValueError:
+    except:
         res = minimize(SS_trc_logis, x0=[10,4], method = 'Powell',args=(df,), bounds=([-5,30],[3,8]))
-    except ValueError:
-        res = minimize(SS_trc_logis, x0=[15,4], method = 'Powell',args=(df,), bounds=([-5,30],[3,8]))
+    except:
+        res = minimize(SS_trc_logis, x0=[20,4], method = 'Powell',args=(df,), bounds=([-5,30],[3,8]))
     return(res)
