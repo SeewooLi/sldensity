837b6d21974ee4070bc5a3305dc547c7676dcc17
이시우 Fredric <cu.li@fadutec.com>
2023-01-11 10:02:42 +0900

    pv0 range

--- build/lib/sldensity/PRHL.py
+++ build/lib/sldensity/PRHL.py
@@ -167,7 +167,7 @@ def qlog(q,m,s):
     return(x)
 
 def df0(df):
-    df_ = np.vstack((df.bias_idx_num.iloc[5:50],(df.cell_cnt.cumsum()/df.cell_cnt.sum()).iloc[5:50]))
+    df_ = np.vstack((df.bias_idx_num.iloc[5:65],(df.cell_cnt.cumsum()/df.cell_cnt.sum()).iloc[5:65]))
     return(df_)
                     
 def SS_trc_logis(par, df):
