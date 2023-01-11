42ebc70bc2bdbea4810050a2b0701ec83eeb56b8
이시우 Fredric <cu.li@fadutec.com>
2023-01-11 12:59:50 +0900

    PV0 bounds

--- build/lib/sldensity/PRHL.py
+++ build/lib/sldensity/PRHL.py
@@ -186,7 +186,7 @@ def SS_trc_logis(par, df):
 
 def trc_logis(df):
     df = df0(df)
-    res = minimize(SS_trc_logis, x0=[5,4], method = 'Powell',args=(df,), bounds=([-5,50],[1,80]))
+    res = minimize(SS_trc_logis, x0=[5,4], method = 'Powell',args=(df,), bounds=([-15,50],[1,80]))
     return(res)
 
 ############################################################
